f202906f75d82f3be2ce68b3ba81c06f78ce1b1c
peter <peter.fischer@fau.de>
2019-04-12 22:08:29 +0200

    add gitignore and move file to python

---
+++ python/image_extract.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+FFMPEG_BIN = "ffmpeg.exe" # on Windows
+import subprocess as sp
+command = [
+		FFMPEG_BIN,
+		'-i', r"C:\Users\Peter\Desktop\20130208020906.MTS",
+        '-f', 'image2pipe',
+        '-pix_fmt', 'rgb24', '-']
+pipe = sp.Popen(command, stdout = sp.PIPE, bufsize=10**8)
+import numpy as np
+raw_image = pipe.stdout.read(420*360*3)
+import numpy
+# read 1 frame
+raw_image = pipe.stdout.read(1440*1080*3)
+image =  numpy.fromstring(raw_image, dtype='uint8')
+image = image.reshape((1080,1440,3))
+import matplotlib.pyplot as plt
+plt.imshow(image)
+plt.show()
